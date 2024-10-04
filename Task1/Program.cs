ed06fa215377baf0952b0e700f1884bcc5b123d7
e.goncharenko <e.goncharenko@GR-ILSKIY-PC212>
2024-10-04 09:33:36 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static double GetSquare(double x, double y, double z, double p)
+    {
+        return Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длины сторон первого треугольника");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        double z1 = Convert.ToDouble(Console.ReadLine());
+        double p1 = (x1 + y1 + z1) / 2;
+        double s1 = GetSquare(x1, y1, z1, p1);
+        Console.WriteLine("Введите длины сторон второго треугольника");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        double y2 = Convert.ToDouble(Console.ReadLine());
+        double z2 = Convert.ToDouble(Console.ReadLine());
+        double p2 = (x2 + y2 + z2) / 2;
+        double s2 = GetSquare(x2, y2, z2, p2);
+
+        if (s1 > s2)
+        {
+            Console.WriteLine("Площадь первого треугольника больше");
+        }
+        else
+        {
+            if (s2 > s1)
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+        }
+    }
+            
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void GetVolumeSquare(double x, out double v,out double s)
+    {
+        v = x * x * x;
+        s = 6 * x * x;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину ребра куба");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double v;
+        double s;
+        GetVolumeSquare(a, out v, out s);
+        Console.WriteLine("{0} {1}", v,s);
+
+    }
+}
